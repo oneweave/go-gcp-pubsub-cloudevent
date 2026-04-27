9cfc1022c7146447f3ff184bdf5dc79a64083e23
Stefan Pfaffel <s.pfaffel@gmail.com>
2026-04-27 21:06:32 +0200

    feat: add high-level exports of ctors
    Signed-off-by: Stefan Pfaffel <s.pfaffel@gmail.com>

--- pubsub.go
+++ pubsub.go
@@ -1,8 +1,25 @@
 package oneweavepubsub
 
-import "github.com/oneweave/oneweave-pubsub/produce"
+import (
+	"github.com/oneweave/oneweave-pubsub/consume"
+	"github.com/oneweave/oneweave-pubsub/produce"
+)
+
+type HTTPConsumer = consume.HTTPConsumer
+type PubSubHTTPConsumer = consume.PubSubHTTPConsumer
+type PubSubHTTPConsumerConfig = consume.PubSubHTTPConsumerConfig
 
 // NewPublisher creates a high-level publisher configured for CloudEvent output.
 func NewPublisher(config produce.Config, sender produce.Sender) (*produce.Publisher, error) {
 	return produce.NewPublisher(config, sender)
+}
+
+// NewHTTPConsumer creates a CloudEvent HTTP consumer.
+func NewHTTPConsumer() *consume.HTTPConsumer {
+	return consume.NewHTTPConsumer()
+}
+
+// NewPubSubHTTPConsumer creates a Pub/Sub push HTTP consumer.
+func NewPubSubHTTPConsumer(config consume.PubSubHTTPConsumerConfig) (*consume.PubSubHTTPConsumer, error) {
+	return consume.NewPubSubHTTPConsumer(config)
 }
